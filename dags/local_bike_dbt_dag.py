ef7dfca5d38a5780a5ded63b2d953307feae646b
Jeremy Marchandeau <jerem9911@hotmail.com>
2026-06-15 14:29:46 +0200

    Refactor local_bike_dbt_dag to use DbtTaskGroup and restructure DAG definition

--- dags/local_bike_dbt_dag.py
+++ dags/local_bike_dbt_dag.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
-from cosmos import DbtDag, ExecutionConfig, ProfileConfig, ProjectConfig, RenderConfig
+from airflow.decorators import dag
+from airflow.operators.empty import EmptyOperator
+from cosmos import DbtTaskGroup, ExecutionConfig, ProfileConfig, ProjectConfig, RenderConfig
 from cosmos.constants import LoadMode
 from pendulum import datetime
 
@@ -13,23 +15,42 @@ profile_config = ProfileConfig(
     profiles_yml_filepath=DBT_PROFILES_PATH / "profiles.yml",
 )
 
-dbt_dag = DbtDag(
+project_config = ProjectConfig(
+    dbt_project_path=DBT_PROJECT_PATH,
+    install_dbt_deps=True,
+)
+
+execution_config = ExecutionConfig(
+    dbt_executable_path="/usr/local/bin/dbt",
+)
+
+render_config = RenderConfig(
+    load_method=LoadMode.DBT_LS,
+)
+
+
+@dag(
     dag_id="local_bike_dbt",
-    project_config=ProjectConfig(
-        dbt_project_path=DBT_PROJECT_PATH,
-        install_dbt_deps=True,
-    ),
-    profile_config=profile_config,
-    execution_config=ExecutionConfig(
-        dbt_executable_path="/usr/local/bin/dbt",
-        dbt_cmd_global_flags=["--full-refresh"],
-    ),
-    render_config=RenderConfig(
-        load_method=LoadMode.DBT_LS,
-    ),
     schedule="@daily",
     start_date=datetime(2025, 1, 1),
     catchup=False,
     default_args={"retries": 2},
     tags=["dbt", "bigquery"],
 )
+def local_bike_pipeline():
+    start = EmptyOperator(task_id="start")
+
+    dbt_group = DbtTaskGroup(
+        group_id="dbt_transformations",
+        project_config=project_config,
+        profile_config=profile_config,
+        execution_config=execution_config,
+        render_config=render_config,
+    )
+
+    end = EmptyOperator(task_id="end")
+
+    start >> dbt_group >> end
+
+
+local_bike_pipeline()
